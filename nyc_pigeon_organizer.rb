f0c45bcd78431de71bf9430bd5f20adf064ae2d6
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-09 16:16:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require"pry"
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |attribute, specific_attribute|
-    specific_attribute.each do |colour, name|
+    specific_attribute.each do |second_attribute, name|
       i = 0
       
       if pigeon_list == name[i] && pigeon_list == attribute
