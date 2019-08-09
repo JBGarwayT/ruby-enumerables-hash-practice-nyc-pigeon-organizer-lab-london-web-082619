9f9d4cf475ebe1a79e70c554a8a4dd61df356f12
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-09 17:09:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,8 +12,7 @@ def nyc_pigeon_organizer(data)
     if !pigeon_list[name[i]][attribute]
       pigeon_list[name[i]][attribute] = []
     end
-    pigeon_list[name[i]][attribute] << second_attribute
-     
+    pigeon_list[name[i]][attribute] << second_attribute.to_string
      i += 1
    end
     end
