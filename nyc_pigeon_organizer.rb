0778b203cc7193c0044acc5f416d3999dea7be5d
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-09 16:55:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,10 @@ def nyc_pigeon_organizer(data)
   i = 0
   data.each do |attribute, specific_attribute|
     specific_attribute.each do |second_attribute, name|
-     while i < name.lengthbinding.pry
-     pigeon_list[name] = 
+     while i < name.length
+     binding.pry
+     pigeon_list[name] = attribute
      i += 1
-     
    end
     end
   end
