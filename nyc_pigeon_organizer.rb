7845bc1b5394ede1f5e4460b44b9561d3476942c
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-09 16:51:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,10 @@
 require"pry"
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
- 
   data.each do |attribute, specific_attribute|
     i = 0
     specific_attribute.each do |second_attribute, name|
       binding.pry
-      
-      if pigeon_list == name[i] && pigeon_list == attribute
-        
-      else
-      while i < name.length
-      #pigeon_list[name[i]][attribute] = [second_attribute]
-      
-      i += 1
-    end
-    end
-           
-
     end
   end
   pigeon_list
